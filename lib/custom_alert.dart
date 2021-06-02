4e43cc86704ce7e76fea1d0fb79c3262f9623fbb
mr_president_white <weiss@aw96.de>
2021-06-02 16:26:35 +0200

    Initial commit

---
+++ lib/custom_alert.dart
@@ -0,0 +1,6 @@
+library custom_alert;
+
+export 'src/alert.dart';
+export 'src/alert_style.dart';
+export 'src/constants.dart';
+export 'src/dialog_button.dart';
---
+++ test/custom_alert_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:custom_alert/custom_alert.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+  });
+}
